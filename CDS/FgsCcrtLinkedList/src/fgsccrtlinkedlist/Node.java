a48c3dfeb6c5e9edd9e75b771e2a6f6b87106d33
Rahul <rahulsinghal19@gmail.com>
2014-08-29 16:16:20 -0500

    Concurrent Linked List
    Added coarse grind & fine grained Sync concurrent linked list.

---
+++ CDS/FgsCcrtLinkedList/src/fgsccrtlinkedlist/Node.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fgsccrtlinkedlist;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author Rahul
+ */
+public class Node {
+    int key;
+    Node next;
+    Lock lock;
+    
+    Node(int keyValue) {
+        key = keyValue;
+        lock = new ReentrantLock();
+        next = null;
+    }
+}
